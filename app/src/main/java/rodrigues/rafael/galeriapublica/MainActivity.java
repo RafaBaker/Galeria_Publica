3822a1e6ff341431d959ce5359006632cb6dabf2
20201TIIMI0179 <rafael1309mt@gmail.com>
2022-09-16 13:55:04 -0300

    Passo 6 finalizado

--- app/src/main/java/rodrigues/rafael/galeriapublica/MainActivity.java
+++ app/src/main/java/rodrigues/rafael/galeriapublica/MainActivity.java
@@ -1,14 +1,56 @@
 package rodrigues.rafael.galeriapublica;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationItemView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final MainViewModel vm = new ViewModelProvider(this).get(MainViewModel.class);
+
+        bottomNavigationItemView = findViewById(R.id.btNav);
+        bottomNavigationItemView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                vm.setNavigationOpSelected(item.getItemId());
+                switch (item.getItemId()) {
+                    case R.id.gridViewOp:
+                        GridViewFragment gridViewFragment = GridViewFragment.newInstance();
+                        setFragment(gridViewFragment);
+                        break;
+                    case R.id.listViewOp:
+                        ListViewFragment listViewFragment = ListViewFragment.newInstance();
+                        setFragment(listViewFragment);
+                        break;
+                }
+                return true;
+            }
+        });
+
     }
+
+    void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragContainer, fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+
+    }
+
 }
